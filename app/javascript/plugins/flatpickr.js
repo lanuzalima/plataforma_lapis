f09f5281a7b54cc1f1b00d4042b0941696f8522e
lanuzalima <lanuzalima@gmail.com>
2020-12-15 13:42:09 -0300

    correção configurações do calendário

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,15 +1,21 @@
 import flatpickr from "flatpickr";
-flatpickr(".datepicker", {
-  dateFormat: 'd/m/Y',
-  locale: {
-    firstDayOfWeek: 0,
-    weekdays: {
-      shorthand: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
-      longhand: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],         
-    },
-    months: {
-      shorthand: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Аgo', 'Set', 'Оut', 'Nov', 'Dez'],
-      longhand: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-    },
-  }, 
+
+const calendar = () => {
+  flatpickr(".datepicker", {
+    dateFormat: 'd/m/Y',
+    locale: {
+      firstDayOfWeek: 0,
+      weekdays: {
+        shorthand: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+        longhand: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],         
+      },
+      months: {
+        shorthand: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Аgo', 'Set', 'Оut', 'Nov', 'Dez'],
+        longhand: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+      },
+    }, 
 });
+}
+
+export { calendar };
+
