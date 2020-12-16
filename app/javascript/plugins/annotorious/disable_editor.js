b1a01b947091f5efe746962313ea41e6a90ffab2
Gustavo Sousa <gussousa@gmail.com>
2020-12-16 15:05:00 -0300

    Bugs do zoom corrigidos -finalmente. E agora imagem sempre fica em orientação de retrato.

--- app/javascript/plugins/annotorious/disable_editor.js
+++ app/javascript/plugins/annotorious/disable_editor.js
@@ -1,56 +1,53 @@
 import {commentPop} from './comment_pop'
+import $, { off } from 'jquery';
 
 // Disabilita o editor
 // Procura pelo annotation layer e o remove
 // Recriar o annotation layer sem as annotations e mantendo apenas as formas salvas
 
 const disableEditor = (anno) => {
-  
-  const rects = document.querySelectorAll('rect');
   const viewBox = document.querySelector('.a9s-annotationlayer');
-  const polys = document.querySelectorAll('polygon');
-  let layer = document.querySelector('.a9s-annotationlayer');
   const annotable = document.getElementById('annotable');
-  layer.parentNode.removeChild(layer)
+  const annots = document.querySelectorAll('.a9s-annotation')
 
-  if (viewBox.viewBox == null) {
-    annotable.insertAdjacentHTML('beforebegin', `<svg class="a9s-annotationlayer" 
-    viewBox="${viewBox.attributes[0].ownerElement.viewBox.baseVal.y} 
-    ${viewBox.attributes[0].ownerElement.viewBox.baseVal.x} 
-    ${viewBox.attributes[0].ownerElement.viewBox.baseVal.width} 
-    ${viewBox.attributes[0].ownerElement.viewBox.baseVal.height} 
-    "
-    style="cursor: default"><g> </g></svg>`)
-  } else {
-    annotable.insertAdjacentHTML('beforebegin', `<svg class="a9s-annotationlayer" 
-    viewBox="${parseInt(viewBox.viewBox.textContent)}
-    "
-    style="cursor: default"><g></g></svg>`)
-  }
-
-
-  layer = document.querySelector('.a9s-annotationlayer').firstChild;
-  rects.forEach(rect => {
-    const dataId = rect.parentElement.dataset.id
+  annots.forEach(annot => {
+    const dataId = annot.dataset.id
     const comment = document.querySelectorAll(`[data-cmref*="${dataId}"]`)[0];
     const commentContent = comment.querySelector(".comment_text").textContent;
-    const rectClass = rect.attributes.class.value;
-    const rectX = rect.attributes.x.value;
-    const rectY = rect.attributes.y.value;
-    const rectWidth = rect.attributes.width.value;
-    const rectHeight = rect.attributes.height.value;
-    layer.insertAdjacentHTML('afterbegin', `  <a class="goto_comment" title="${commentContent}" href="#COMENT${dataId}"><g class="a9s-annotation" data-id="${dataId}"> <rect class="${rectClass}"  x="${rectX}" y="${rectY}" width="${rectWidth}" height="${rectHeight}" > </rect> </g>${comment}</a>`)
-  });
 
-  polys.forEach(polygon => {
-    const dataId = polygon.parentElement.dataset.id
-    const comment = document.querySelectorAll(`[data-cmref*="${dataId}"]`)[0];
-    const commentContent = comment.querySelector(".comment_text").textContent;
-    const polyClass = polygon.attributes.class.value;
-    const polyPoints = polygon.attributes.points.value;
-    layer.insertAdjacentHTML('afterbegin', ` <a class="goto_comment" title="${commentContent}" href="#COMENT${dataId}"> <g class="a9s-annotation" data-id="${dataId}"> <polygon class="${polyClass}"  points="${polyPoints}" > </polygon> </g> </a>`)
-  });
+    annot.insertAdjacentHTML('beforebegin', `<a class="goto_comment" title="${commentContent}" href="#COMENT${dataId}" reft="${dataId}"></a>`)
+
+    const link = document.querySelectorAll(`[href*="#COMENT${dataId}"]`)[0];
+    link.appendChild(annot)
+    const annoClone = annot.cloneNode(true);
+    annot.parentNode.replaceChild(annoClone, annot)
+
+  })
+    
+  viewBox.setAttribute('style', 'cursor: default')
+
+  const links = document.querySelectorAll(".goto_comment");
   
+  links.forEach(link =>{
+    link.addEventListener('click', event => {
+      event.preventDefault();
+      const comments = document.querySelectorAll(".coment");
+      const comment = document.querySelector(`[data-cmref*="${link.attributes.reft.value}"]`);
+      comments.forEach(comment => {
+        if (comment.classList.length > 1) {
+          comment.classList.remove("active")
+        }
+      })
+      comment.classList.add("active")
+      comment.tabIndex = -1
+      comment.focus();
+    })
+  })
+  
+  const editorDiv = annotable.parentElement.querySelector("div");
+  editorDiv.parentElement.removeChild(editorDiv)
+
+
   commentPop();
 }
 
--- app/javascript/plugins/annotorious/tools.js
+++ app/javascript/plugins/annotorious/tools.js
@@ -2,14 +2,47 @@ import { Annotorious } from '@recogito/annotorious';
 import { setAnno } from './find_annotable';
 
 const tools = (anno, userRole) => {
+  
+  const zoomInBtn = document.getElementById("tools_zoom_in")
+  const zoomOutBtn = document.getElementById("tools_zoom_out")
+  const zoomReset = document.getElementById("tools_zoom_reset")
+  const img = document.getElementById("annotable")
+
+  img.style.zoom = "100%";
+  const getZoom = () => {
+    return parseInt(img.style.zoom)
+  }
+  // img.style.width = "100%";
+
+  zoomReset.addEventListener('click', event => {
+    event.preventDefault();
+    img.style.zoom = "100%";
+    // img.style.width = "100%";
+    img.attributes.style
+  })
+
+  zoomInBtn.addEventListener('click', event => {
+    event.preventDefault();
+    const zoomIn = parseInt(img.style.zoom) + 10 + '%';
+    img.style.zoom = zoomIn;
+  })
+  
+  zoomOutBtn.addEventListener('click', event => {
+    event.preventDefault();
+    const currentZoom = getZoom();
+    if (currentZoom > 10) {
+      const zoomOut = currentZoom - 10 + '%';
+      img.style.zoom = zoomOut;
+    } else {
+      console.log("Else")
+      img.style.zoom = '10%';
+    }
+  })
+  
   if (userRole == "Teacher" || userRole == "Professor") {
     const rectBtn = document.getElementById("tools_rect")
     const polygonBtn = document.getElementById("tools_polygon")
-    const zoomInBtn = document.getElementById("tools_zoom_in")
-    const zoomOutBtn = document.getElementById("tools_zoom_out")
-    const zoomReset = document.getElementById("tools_zoom_reset")
-    const page = document.getElementById("annotable")
-    
+
     rectBtn.addEventListener('click', event => {
       event.preventDefault();
       anno.destroy();
@@ -31,26 +64,6 @@ const tools = (anno, userRole) => {
       const shape = 'polygon'
       setAnno(polyAnno, shape)
     })
-
-    page.style.zoom = "100%";
-
-    zoomReset.addEventListener('click', event => {
-      event.preventDefault();
-      page.style.zoom = "100%";
-    })
-
-    zoomInBtn.addEventListener('click', event => {
-      event.preventDefault();
-      const zoomIn = parseInt(page.style.zoom) + 10 + '%';
-      page.style.zoom = zoomIn;
-    })
-
-    zoomOutBtn.addEventListener('click', event => {
-      event.preventDefault();
-      const zoomOut = parseInt(page.style.zoom) - 10 + '%';
-      page.style.zoom = zoomOut;
-    })
-
 
   }
 }
