bc012304479f5fe65a13cd13efeae5809b0456aa
lanuzalima <lanuzalima@gmail.com>
2021-02-15 20:08:58 -0300

    não permitir envio de proposta vencida

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -33,7 +33,12 @@ class TextsController < ApplicationController
 
   def new
     @theme = Theme.find(params[:theme_id])
-    @text = Text.new
+    if @theme.end_date > Date.today
+      @text = Text.new
+    else
+      flash[:alert] = "Prazo para o envio desta proposta encerrado"
+      redirect_to themes_path
+    end
   end
 
   def destroy
